c83c8be571146694af1bbc08f96a3e25ba0f675e
Caio Pereira Antunes <caioefg@gmail.com>
2023-01-31 16:57:09 -0300

    feat: implementando a classe simple-report

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,30 @@
+from datetime import date
+from collections import Counter
+
+
 class SimpleReport:
+    @staticmethod
+    def generate(lista):
+        now = str(date.today())
+
+        company = [item["nome_da_empresa"] for item in lista]
+
+        count_company = Counter(company).most_common()[0][0]
+
+        manufacturing_dates = [item["data_de_fabricacao"] for item in lista]
+
+        old_manufacturing_dates = min(manufacturing_dates)
+
+        expired_dates = [
+            item["data_de_validade"] for item in lista
+            if item["data_de_validade"] >= now
+        ]
+
+        next_expired_date = min(expired_dates)
+
+        return (
+            f"Data de fabricação mais antiga: {old_manufacturing_dates}\n"
+            f"Data de validade mais próxima: {next_expired_date}\n"
+            f"Empresa com mais produtos: {count_company}"
+        )
     pass
