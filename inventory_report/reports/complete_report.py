20169c9e70f0af772dbe32689319d992840989ef
Caio Pereira Antunes <caioefg@gmail.com>
2023-01-31 18:44:57 -0300

    feat: Implementando CompleteReport generate

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,21 @@
-class CompleteReport:
+from inventory_report.reports.simple_report import SimpleReport
+
+
+# https://pencilprogrammer.com/python-self-vs-cls/#:~:text=cls%20in%20Python%20holds%20the,classmethod%20decorator)%20by%20Python%20itself.&text=It%20is%20important%20to%20note,just%20naming%20conventions%20in%20Python.
+
+
+class CompleteReport(SimpleReport):
     pass
+
+    @classmethod
+    def generate(cls, list):
+        simple_report = super().generate(list)
+
+        companies = cls.get_company(list)
+        # print(companies.items())
+        # separou por par e feito um for
+        simple_report += "\nProdutos estocados por empresa:\n"
+        for company, qnty in companies.items():
+            simple_report += f"- {company}: {qnty}\n"
+        print(simple_report)
+        return simple_report
