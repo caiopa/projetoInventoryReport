c443542ea09184ef93295f691718c52e0e42533a
Caio Pereira Antunes <caioefg@gmail.com>
2023-01-31 21:30:29 -0300

    Feat: criando metodo importData - extensao e tipo

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,22 @@
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.importer.json_importer import JsonImporter
+
+
+class Inventory:
+
+    @classmethod
+    def import_data(path: str, type: str):
+        if "csv" in path:
+            data = CsvImporter.import_data(path)
+        if "json" in path:
+            data = JsonImporter.import_data(path)
+        if "xml" in path:
+            data = XmlImporter.import_data(path)
+
+        return (
+            SimpleReport(data) if type == "simples"
+            else CompleteReport(data)
+        )
