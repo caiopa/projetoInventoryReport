7b7a50cea552dea09f77982b08309b385d1ccf89
Caio Pereira Antunes <caioefg@gmail.com>
2023-02-01 13:20:15 -0300

    bug fix: Arrumando retorno esperando pela funcao

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -8,7 +8,7 @@ from inventory_report.importer.json_importer import JsonImporter
 class Inventory:
 
     @classmethod
-    def import_data(path: str, type: str):
+    def import_data(cls, path: str, type: str):
         if "csv" in path:
             data = CsvImporter.import_data(path)
         if "json" in path:
@@ -17,6 +17,6 @@ class Inventory:
             data = XmlImporter.import_data(path)
 
         return (
-            SimpleReport(data) if type == "simples"
-            else CompleteReport(data)
+            SimpleReport.generate(data) if type == "simples"
+            else CompleteReport.generate(data)
         )
